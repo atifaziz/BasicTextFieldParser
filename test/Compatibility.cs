bc8855700577b6689a0c8b81f36efc42728b403c
Atif Aziz <code@raboof.com>
2018-04-13 12:00:19 +0200

    Initial fork from Mono's implementation
    mono/mono-basic@23f11ff9f636e5929f90e9150e77ad2f31455b26

---
+++ test/Compatibility.cs
@@ -0,0 +1,37 @@
+﻿namespace Microsoft.VisualBasic.FileIO
+{
+    using System.IO;
+
+    static class FileSystem
+    {
+        public static void WriteAllText(string file, string text, bool append)
+        {
+            if (append)
+                File.AppendAllText(file, text);
+            else
+                File.WriteAllText(file, text);
+        }
+
+        public static void WriteAllBytes(string file, byte[] data, bool append)
+        {
+            using (var fs = File.OpenWrite(file))
+            {
+                if (append)
+                    fs.Seek(0, SeekOrigin.End);
+                fs.Write(data, 0, data.Length);
+            }
+        }
+    }
+}
+
+namespace Microsoft.VisualBasic
+{
+    static class Strings
+    {
+        public static string Join(string[] array, string delimiter) =>
+            (array?.Length ?? 0) == 0 ? null : string.Join(delimiter ?? " ", array);
+
+        public static string Join(object[] array, string delimiter) =>
+            (array?.Length ?? 0) == 0 ? null : string.Join(delimiter ?? " ", array);
+    }
+}
---
+++ test/Helper.cs
@@ -0,0 +1,20 @@
+﻿namespace MonoTests.Microsoft_VisualBasic.FileIO
+{
+    using System;
+
+    static class Helper
+    {
+        public static void RemoveWarning  (object obj)
+        {
+        }
+
+        public static string Join <T> (T [] array, string delimiter)
+        {
+            if (array == null)
+                return Microsoft.VisualBasic.Strings.Join (null, delimiter);
+            object [] obj = new object [array.Length];
+            Array.Copy (array, obj, array.Length);
+            return Microsoft.VisualBasic.Strings.Join (obj, delimiter);
+        }
+    }
+}
